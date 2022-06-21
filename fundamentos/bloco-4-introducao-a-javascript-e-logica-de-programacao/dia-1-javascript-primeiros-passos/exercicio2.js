84558a7a5c39019a508452e64487750077371d95
carlosbprado <carlosbprado@outlook.com>
2022-06-21 19:00:07 -0300

    dia 1 bloco 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio2.js
@@ -0,0 +1,9 @@
+const a = 13;
+const b = 8;
+
+if (a > b) {
+    console.log('a e maior que b');
+}
+else {
+    console.log('b e maior que a');
+}
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio3.js
@@ -0,0 +1,13 @@
+const a = 35;
+const b = 21;
+const c = 12;
+
+if (a > b && a> c) {
+    console.log('A e o maior numero');
+}
+else if (b > a && b > c){
+    console.log('B e o maior numero');
+}
+else {
+    console.log('C e o maior numero');
+}
