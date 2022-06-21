9ef03b7dfdcf199a5d777f0ff43bd349802aa288
carlosbprado <carlosbprado@outlook.com>
2022-06-21 19:10:33 -0300

    exercicio 5

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio5.js
@@ -0,0 +1,12 @@
+const anguloA = 10;
+const anguloB = 70;
+const anguloC = 100;
+
+let somaAngulos = anguloA + anguloB + anguloC;
+
+if(somaAngulos === 180){
+    console.log(true);
+}
+else {
+    console.log(false);
+}
