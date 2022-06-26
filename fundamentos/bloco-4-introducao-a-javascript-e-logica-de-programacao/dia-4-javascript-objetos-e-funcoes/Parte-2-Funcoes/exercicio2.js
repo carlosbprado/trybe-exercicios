fc0b374268931d75c1cf5ee91501ed4934701826
carlosbprado <carlosbprado@outlook.com>
2022-06-26 20:48:17 -0300

    exercicio 2 concluido

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Parte-2-Funcoes/exercicio2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Parte-2-Funcoes/exercicio2.js
@@ -1,3 +1,23 @@
 // 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 // Array de teste: [2, 3, 6, 7, 10, 1];.
 // Valor esperado no retorno da função: 4.
+
+// function indiceMaior(numeros) {
+//   let apoio = 0;
+//   for (let a in numeros) {
+//     console.log(numeros[a]);
+//   }
+// }
+// indiceMaior([2, 3, 6, 7, 10, 1]);
+// Aqui retornamos os itens do array
+
+function indiceMaior(numeros) {
+    let apoio = 0;
+    for (let a in numeros) {
+      if(numeros[apoio] < numeros[a]){
+        apoio = a
+      }
+    }
+    console.log(apoio);
+  }
+  indiceMaior([2, 3, 6, 7, 10, 1]);
