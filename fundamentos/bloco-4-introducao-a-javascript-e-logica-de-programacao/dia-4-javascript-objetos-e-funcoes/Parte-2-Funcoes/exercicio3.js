8da86855217e9b3b028f934447975495cb78880c
carlosbprado <carlosbprado@outlook.com>
2022-06-26 20:59:11 -0300

    exercicio 3 concluido

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Parte-2-Funcoes/exercicio3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Parte-2-Funcoes/exercicio3.js
@@ -1,3 +1,14 @@
 // 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 // Array de teste: [2, 4, 6, 7, 10, 0, -3];.
 // Valor esperado no retorno da função: 6.
+
+function menorValor (numeros){
+    let apoio = 0;
+    for(a in numeros){
+        if(numeros[apoio] > numeros[a]){
+            apoio = a
+        }
+    }
+    console.log(apoio); // exercicio pede para retornar mas visualmente o console.log consigo ver no terminal o que esta acontecendo.
+}
+menorValor([2, 4, 6, 7, 10, 0, -3])
